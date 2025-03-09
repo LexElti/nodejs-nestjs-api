20b5dd0bfde77099a8307c9ba240a1662df8fe0f
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-09 14:56:43 +0300

    Тесты логина

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { disconnect } from 'mongoose';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+
+const loginDto: AuthDto = {
+  login: 'test@mail.ru',
+  password: '123',
+};
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/login (POST) - success', (done) => {
+    request(app.getHttpServer())
+      .post('/auth/login')
+      .send(loginDto)
+      .expect(200)
+      .then(({ body }: request.Response) => {
+        expect(body.access_token).toBeDefined();
+        done();
+      })
+      .catch(() => {});
+  });
+
+  it('/auth/login (POST) - fail password', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ ...loginDto, password: '2' })
+      .expect(401, {
+        statusCode: 401,
+        message: 'Неверный пароль',
+        error: 'Unauthorized',
+      });
+  });
+
+  it('/auth/login (POST) - fail email', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ ...loginDto, login: 'aaa@a.ru' })
+      .expect(401, {
+        statusCode: 401,
+        message: 'Пользователь с таким email не найден',
+        error: 'Unauthorized',
+      });
+  });
+
+  afterAll(() => {
+    disconnect();
+  });
+});
