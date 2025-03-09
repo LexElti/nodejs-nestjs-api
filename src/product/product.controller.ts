ac9930f3467d9f0f32e36fe3c0358531bb290dc4
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-09 16:46:57 +0300

    Pipe валидации id

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -15,6 +15,7 @@ import { FindProductDto } from './dto/find-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
+import { IdValidationPipe } from '../pipes/ad-validation.pipe';
 
 @Controller('product')
 export class ProductController {
@@ -26,7 +27,7 @@ export class ProductController {
   }
 
   @Get(':id')
-  async get(@Param('id') id: string) {
+  async get(@Param('id', IdValidationPipe) id: string) {
     const product = await this.productService.findById(id);
     if (!product) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
@@ -35,7 +36,7 @@ export class ProductController {
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedProduct = await this.productService.deleteById(id);
     if (!deletedProduct) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
@@ -43,7 +44,10 @@ export class ProductController {
   }
 
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: CreateProductDto) {
+  async patch(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: CreateProductDto,
+  ) {
     const updatedProduct = await this.productService.updateById(id, dto);
     if (!updatedProduct) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
