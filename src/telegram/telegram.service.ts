f774c74f9ec89150b4550a84a77add9b4089cd8f
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-10 23:14:13 +0300

    Реализация forRootAsync

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Telegraf } from 'telegraf';
+import { TELEGRAM_MODULE_OPTIONS } from './telegram.constants';
 import { ITelegramOptions } from './telegram.interface';
-import { getTelegramConfig } from '../configs/telegram.config';
 
 @Injectable()
 export class TelegramService {
   bot: Telegraf;
   options: ITelegramOptions;
 
-  constructor() {
-    this.bot = new Telegraf(this.options.token);
-    this.options = getTelegramConfig();
+  constructor(@Inject(TELEGRAM_MODULE_OPTIONS) options: ITelegramOptions) {
+    this.bot = new Telegraf(options.token);
+    this.options = options;
   }
 
   async sendMessage(message: string, chatId: string = this.options.chatId) {
